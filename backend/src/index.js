2be031075ad1db1c25a125e44227542bde84b720
togana <o.togana@gmail.com>
2018-06-17 13:57:44 +0900

    Prisma と backend をつないで利用できるクエリを絞った

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,53 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-const typeDefs = `
-type Query {
-  description: String
-}
-`;
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
+    posts(parent, args, ctx, info) {
+      return ctx.db.query.posts({ }, info);
+    },
+    post(parent, args, ctx, info) {
+      return ctx.db.query.post({ where: { id: args.id } }, info);
+    },
     description: () => 'This is the API for a simple blogging application',
+  },
+  Mutation: {
+    createDraft(parent, { title, content }, ctx, info) {
+      return ctx.db.mutation.createPost(
+        {
+          data: {
+            title,
+            content,
+          },
+        },
+        info,
+      );
+    },
+    deletePost(parent, { id }, ctx, info) {
+      return ctx.db.mutation.deletePost({ where: { id } }, info);
+    },
+    publish(parent, { id }, ctx, info) {
+      return ctx.db.mutation.updatePost(
+        {
+          where: { id },
+          data: { published: true },
+        },
+        info,
+      );
+    },
   },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'http://localhost:4466',
+    }),
+  }),
 });
 
-server.start(() => console.log('The server is running on http://localhost:4000'));
+server.start(() => console.log('Server is running on http://localhost:4000'));
