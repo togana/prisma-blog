065fc39f8295cdd6059f793e34a78c5c9fb99ef0
togana <o.togana@gmail.com>
2018-07-01 17:22:42 +0900

    withRouter を pages 側で呼び出すようにした

--- front/components/Feed.js
+++ front/components/Feed.js
@@ -12,49 +12,49 @@ const FEED = gql`
   }
 `;
 
-export default function Feed() {
-  return (
-    <Query query={FEED}>
-      {({ loading, error, data }) => {
-        if (loading) {
-          return (
-            <aside>
-              is loading.
-            </aside>
-          );
-        }
-
-        if (error) {
-          return (
-            <aside>
-              Error loading feed.
-            </aside>
-          );
-        }
-
-        const { feed } = data;
+const Feed = () => (
+  <Query query={FEED}>
+    {({ loading, error, data }) => {
+      if (loading) {
         return (
-          <div>
-            <h1>
-              Feed
-            </h1>
-            {feed.map(post => (
-              <div key={post.id}>
-                <Link
-                  href={{
-                    pathname: '/post',
-                    query: { id: post.id },
-                  }}
-                >
-                  <a>
-                    { post.title }
-                  </a>
-                </Link>
-              </div>
-            ))}
-          </div>
+          <aside>
+            is loading.
+          </aside>
         );
-      }}
-    </Query>
-  );
-}
+      }
+
+      if (error) {
+        return (
+          <aside>
+            Error loading feed.
+          </aside>
+        );
+      }
+
+      const { feed } = data;
+      return (
+        <div>
+          <h1>
+            Feed
+          </h1>
+          {feed.map(post => (
+            <div key={post.id}>
+              <Link
+                href={{
+                  pathname: '/post',
+                  query: { id: post.id },
+                }}
+              >
+                <a>
+                  { post.title }
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
+      );
+    }}
+  </Query>
+);
+
+export default Feed;
--- front/pages/post.js
+++ front/pages/post.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { withRouter } from 'next/router';
 import Header from '../components/Header';
 import Post from '../components/Post';
 
-export default props => (
+export default withRouter(props => (
   <div>
     <Header />
     <Post
-      id={props.id}
+      id={props.router.query.id}
     />
   </div>
-);
+));
