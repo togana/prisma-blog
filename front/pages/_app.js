af2a8f30d0b0292cedbe1afffc276876274634ac
togana <o.togana@gmail.com>
2018-07-01 14:29:56 +0900

    ApolloProvider の ラッパー書いた

---
+++ front/pages/_app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+import withApollo from '../lib/withApollo';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, apollo } = this.props;
+
+    return (
+      <Container>
+        <ApolloProvider client={apollo}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </Container>
+    );
+  }
+}
+
+export default withApollo(MyApp);
